d7749ca4415df2c5db7e709f5ef8952108dd8717
motinisi <nisi77851@gmail.com>
2024-10-12 18:59:32 +0900

    j

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: timanish <timanish@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 15:59:51 by timanish          #+#    #+#             */
-/*   Updated: 2024/10/09 18:17:55 by timanish         ###   ########.fr       */
+/*   Updated: 2024/10/11 20:09:02 by timanish         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,39 +26,70 @@
 
 
 
-void	philo_state(void *arg)
-{
+// void	philo_state(void *arg)
+// {
 
-}
+// }
 
-void	philo_routine(void *arg)
+void	*philo_routine(void *arg)
 {
-	t_philo	*p_data;
+	t_philo			*p_data;
+	struct timeval	tv;
 
 	p_data = (t_philo *)arg;
 
-	while(1)
+	while (1)
 	{
-		printf("%d %d is eating\n", )
+		gettimeofday(&tv, NULL);
+		printf("%ld %d is eating\n", tv.tv_usec * 1000 + tv.tv_usec / 1000,
+			p_data->id);
 		usleep(1000 * p_data->eat_time);
 	}
+
 }
 
 int	main(int argc, char **argv)
 {
+	printf("so far okokokok\n");
 	int				total_p;
 	t_philo			*p_data;
 	pthread_t		*p_pthread;
 	pthread_mutex_t	*fork;
-	int				i;
+	size_t			i;
 
-	total_p = argv[1];
+	total_p = ft_atoi(argv[1]);
+	p_data->eat_time = ft_atoi(argv[2]);
 	p_data = (t_philo *)malloc(sizeof(t_philo) * total_p);
 	p_pthread = (pthread_t *)malloc(sizeof(pthread_t) * total_p);
 	fork = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t) * total_p);
 	i = 0;
-
-
-
+	while (i < total_p)
+	{
+		pthread_mutex_init(&fork[i], NULL);
+		i ++;
+	}
+	i = 0;
+	while (i < total_p)
+	{
+		p_data[i].id = i + 1;
+		p_data[i].left_fork = &fork[i - 1];
+		if (i == 0)
+			p_data[i].left_fork = &fork[total_p - 1];
+		p_data[i].right_fork = &fork[i];
+		pthread_create(&p_pthread[i], NULL, philo_routine, &p_data[i]);
+		i ++;
+	}
+	i = 0;
+	while (i < total_p)
+	{
+		pthread_join(p_pthread[i], NULL);
+		i ++;
+	}
+	i = 0;
+	while (i < total_p)
+	{
+		pthread_mutex_destroy(&fork[i]);
+		i ++;
+	}
 	return (0);
 }
--- philo/philo.h
+++ philo/philo.h
@@ -6,14 +6,18 @@
 /*   By: timanish <timanish@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 15:58:09 by timanish          #+#    #+#             */
-/*   Updated: 2024/10/09 18:07:23 by timanish         ###   ########.fr       */
+/*   Updated: 2024/10/11 19:51:00 by timanish         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
 # include <pthread.h>
+# include <sys/time.h>
 
 typedef struct s_philo
 {
@@ -26,6 +30,9 @@ typedef struct s_philo
 	pthread_mutex_t	*left_fork;
 	pthread_mutex_t	*right_fork;
 }	t_philo;
+
+static size_t	ft_strlen_n(const char *str);
+long			ft_atoi(const char *str);
 
 
 #endif
